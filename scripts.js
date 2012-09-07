f35295f23c88a49ac7368d33a98dea8258da5e79
Salvatore Rinchiera <srinchiera@college.harvard.edu>
2012-09-06 22:34:09 -0400

    email validation

--- scripts.js
+++ scripts.js
@@ -20,6 +20,11 @@ $(document).ready(function(){
 	);
 });
 
+function validEmail(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
+
 function resetForm(){
 	$('#email').val('');
 	$('#fullname').val('');
@@ -35,6 +40,11 @@ function resetForm(){
 
 function subscribe () {
 	var email = $('#email').val();
+    if (!validEmail(email)){
+        $('#test').html('<div id="badEmail" style="display:none" class="alert alert-error"> <a class="close" data-dismiss="alert">×</a><strong>Error! </strong>Invalid email, you noob. </div>');
+    	$('#badEmail').toggle();
+        return;
+    }
 	var fullname = $('#fullname').val();
 	var lists = [];
 	selectedLists = $('.mlist.active');
@@ -47,7 +57,6 @@ function subscribe () {
 	}
 	for (var i in lists) {
 		listName = lists[i].toLowerCase();
-		console.log(listName);
 		var url = "https://lists.hcs.harvard.edu/mailman/subscribe/hcs-"+listName;
 		var data = "email="+email+"&fullname="+fullname;
 		$.ajax({
